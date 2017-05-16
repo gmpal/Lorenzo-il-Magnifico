4a655f5c5ff5020785616b23d0677873b22b3954
gianmarco <g.m.pal95@gmail.com>
2017-05-16 17:55:49 +0200

    messaggio

--- src/main/java/it/polimi/ingsw/GC_24/values/Value.java
+++ src/main/java/it/polimi/ingsw/GC_24/values/Value.java
@@ -4,19 +4,12 @@ public class Value {
 
 	private int quantity;
 
-<<<<<<< HEAD
+
 	public int getQuantity() {
 		return quantity;
 	}
 
 	public void setQuantity(int quantity) {
-=======
-	public int getValue() {
-		return quantity;
-	}
-
-	public void setValue(int quantity) {
->>>>>>> 6b4804ba2f53c9f383dddbb741e7f2855264d649
 		this.quantity = quantity;
 	}
 	
