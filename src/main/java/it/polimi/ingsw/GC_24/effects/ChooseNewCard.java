bec952210719ed17307aee8f2a668597682aee2b
gmpal <g.m.pal95@gmail.com>
2017-05-21 17:36:34 +0200

    Commit pre-merge con GiorgiaBranch per correzioni su CouncilPrivilege (estensione a ImmediateEffect e non ad Effect)

---
+++ src/main/java/it/polimi/ingsw/GC_24/effects/ChooseNewCard.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.GC_24.effects;
+
+
+/*This effects lets the player choose another card of ONE or MULTI colours, with a specific minimum value.
+ * It's exaclty like putting a familyMember: the player has to pay 3 moneys if tower is occupied,
+ * receives the immediateEffect if the place has it, can increase the value with servants */
+public class ChooseNewCard {
+
+}
