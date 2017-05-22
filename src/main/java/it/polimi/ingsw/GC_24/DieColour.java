e0a34a9eef44f9461319550ec16040775c60ee2b
carlopulv <carlo.pulvirenti@mail.polimi.it>
2017-05-22 08:53:50 +0200

    COmmittin before checkout

--- src/main/java/it/polimi/ingsw/GC_24/DieColour.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.GC_24;
-
-public enum DieColour {
-	BLACK, 
-	ORANGE, 
-	WHITE;
-	
-}
--- src/main/java/it/polimi/ingsw/GC_24/PlayerColour.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.GC_24;
-
-public enum PlayerColour {
-	YELLOW, 
-	GREEN, 
-	BLUE, 
-	RED,
-}
-
