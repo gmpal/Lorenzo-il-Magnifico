72ce1af0678f3aed2c7534f5982af4c11ed7c7e0
carlopulv <carlo.pulvirenti@mail.polimi.it>
2017-05-27 16:17:10 +0200

    fix eccezioni in classe deck

--- src/main/java/it/polimi/ingsw/GC_24/cards/Deck.java
+++ src/main/java/it/polimi/ingsw/GC_24/cards/Deck.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.GC_24.cards;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,55 +13,40 @@ public class Deck {
 	private ArrayList<Buildings> deckBuildings = new ArrayList<>();
 	private ArrayList<Ventures> deckVentures = new ArrayList<>();
 
-	public Deck() {
+	public Deck() throws IOException {
 		createDeck();
 	}
 
-	private void createDeck() {
-		BufferedReader br = null;
+	public void createDeck() throws IOException {
+		BufferedReader br;
 		Gson gson = new Gson();
 		String line;
-		try {
-			br = new BufferedReader(
-					new FileReader("src/main/java/it/polimi/ingsw/GC_24/devCardJsonFile/territoriesCards.json"));
-			while ((line = getLine(br)) != null) {
-				this.deckTerritories.add(gson.fromJson(line, Territories.class));
-			}
-			br = new BufferedReader(
-					new FileReader("src/main/java/it/polimi/ingsw/GC_24/devCardJsonFile/charactersCards.json"));
-			while ((line = getLine(br)) != null) {
-				this.deckCharacters.add(gson.fromJson(line, Characters.class));
-			}
-			br = new BufferedReader(
-					new FileReader("src/main/java/it/polimi/ingsw/GC_24/devCardJsonFile/buildingsCards.json"));
-			while ((line = getLine(br)) != null) {
-				this.deckBuildings.add(gson.fromJson(line, Buildings.class));
-			}
-			br = new BufferedReader(
-					new FileReader("src/main/java/it/polimi/ingsw/GC_24/devCardJsonFile/venturesCards.json"));
-			while ((line = getLine(br)) != null) {
-				this.deckVentures.add(gson.fromJson(line, Ventures.class));
-			}
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				br.close();
-			} catch (IOException ex) {
-				System.err.println("An IOException was caught!");
-				ex.printStackTrace();
-			}
+		br = new BufferedReader(
+				new FileReader("src/main/java/it/polimi/ingsw/GC_24/devCardJsonFile/territoriesCards.json"));
+		while ((line = getLine(br)) != null) {
+			this.deckTerritories.add(gson.fromJson(line, Territories.class));
+		}
+		br = new BufferedReader(
+				new FileReader("src/main/java/it/polimi/ingsw/GC_24/devCardJsonFile/charactersCards.json"));
+		while ((line = getLine(br)) != null) {
+			this.deckCharacters.add(gson.fromJson(line, Characters.class));
+		}
+		br = new BufferedReader(
+				new FileReader("src/main/java/it/polimi/ingsw/GC_24/devCardJsonFile/buildingsCards.json"));
+		while ((line = getLine(br)) != null) {
+			this.deckBuildings.add(gson.fromJson(line, Buildings.class));
+		}
+		br = new BufferedReader(
+				new FileReader("src/main/java/it/polimi/ingsw/GC_24/devCardJsonFile/venturesCards.json"));
+		while ((line = getLine(br)) != null) {
+			this.deckVentures.add(gson.fromJson(line, Ventures.class));
 		}
 	}
 
-	public String getLine(BufferedReader br) {
-		String line = null;
-		try {
-			line = br.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public String getLine(BufferedReader br) throws IOException {
+		String line;
+		line = br.readLine();
 		return line;
 	}
 }
