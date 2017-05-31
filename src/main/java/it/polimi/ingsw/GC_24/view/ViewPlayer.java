b32ec9e780148431f0239a240dcce18d0e0b8c40
carlopulv <carlo.pulvirenti@mail.polimi.it>
2017-05-31 21:56:43 +0200

    aggiunte classi view CLI e GUI

---
+++ src/main/java/it/polimi/ingsw/GC_24/view/ViewPlayer.java
@@ -0,0 +1,24 @@
+﻿package it.polimi.ingsw.GC_24.view;
+
+import it.polimi.ingsw.GC_24.MyObservable;
+
+public abstract class ViewPlayer extends MyObservable {
+		protected String name;
+		protected String colour;
+		
+		public String getName() {
+			return name;
+		}
+		
+		public String getColour() {
+			return colour;
+		}
+		
+		public abstract void start();
+		
+		public abstract String setName();
+		
+		public abstract String setColour();
+		
+		public abstract String createMessage(String name, String Colour);
+}
