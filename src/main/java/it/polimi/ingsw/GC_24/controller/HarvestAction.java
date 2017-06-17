d9359d4ab3eb95022cfd2806772c36b4e20ee4b7
carlopulv <carlo.pulvirenti@mail.polimi.it>
2017-06-17 18:12:17 +0200

    altre modifiche

--- src/main/java/it/polimi/ingsw/GC_24/controller/HarvestAction.java
+++ src/main/java/it/polimi/ingsw/GC_24/controller/HarvestAction.java
@@ -3,18 +3,15 @@ package it.polimi.ingsw.GC_24.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import it.polimi.ingsw.GC_24.cards.Development;
-import it.polimi.ingsw.GC_24.cards.Territories;
 import it.polimi.ingsw.GC_24.effects.ImmediateEffect;
 import it.polimi.ingsw.GC_24.effects.PerformHarvest;
 import it.polimi.ingsw.GC_24.model.Model;
-import it.polimi.ingsw.GC_24.places.CouncilPlace;
 import it.polimi.ingsw.GC_24.places.HarvestPlace;
 
-public class HarvestProductionAction extends Action {
+public class HarvestAction extends Action {
 	private List<ImmediateEffect> immediateEffects = new ArrayList<>();
 
-	public HarvestProductionAction(Model game, String familiar, String zone, String floor, String servants) {
+	public HarvestAction(Model game, String familiar, String zone, String floor, String servants) {
 		super(game, familiar, zone, floor, servants);
 	}
 
