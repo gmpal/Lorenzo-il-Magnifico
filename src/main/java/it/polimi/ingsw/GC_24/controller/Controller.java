8c4e5bf2f6684d206c9494df7edd1490b79d3da5
gianmarco <g.m.pal95@gmail.com>
2017-06-20 12:55:03 +0200

    Players ingresso

--- src/main/java/it/polimi/ingsw/GC_24/controller/Controller.java
+++ src/main/java/it/polimi/ingsw/GC_24/controller/Controller.java
@@ -29,7 +29,7 @@ public class Controller extends MyObservable implements MyObserver {
 	private Action action;
 	private HashMap<String, Object> hashMap;
 	private int controllerNumber = 0;
-	
+
 	// constructor
 
 	public Controller(Model game) {
@@ -73,7 +73,7 @@ public class Controller extends MyObservable implements MyObserver {
 
 		if (command.contains("player")) {
 			try {
-				return handlePlayer(request);
+				return sendPlayerToServer(o, request);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -133,29 +133,37 @@ public class Controller extends MyObservable implements MyObserver {
 		return " ArrayListOfColours sent";
 	}
 
-	private String handlePlayer(Map<String, Object> request) throws InterruptedException {
-		System.out.println("Controller: started handling player!");
-		StringTokenizer tokenizer = new StringTokenizer((String) request.get("player"));
+	private synchronized String sendPlayerToServer(MyObservable o, Map<String, Object> request) throws InterruptedException {
+		System.out.println("Controller #" + this.getControllerNumber() + ": started handling player!");
+		System.out.println("Your player will end in game #" + game.getModelNumber());
+		String playerString = (String) request.get("player");
+		Player player = createPlayerFromString(playerString);
+		System.out.println("Controller: player Created");
+
+		Server.handlePlayer(o, player);
+
+		System.out.println("Controller: state changed: new state");
+		System.out.println(game.getGameState());
+
+		return "Controller: Created player " + player.getMyName().toString() + "\n --> CONNECTED TO GAME N"
+				+ game.getModelNumber() + "\n --> CONNECTED TO CONTROLLER N" + this.getControllerNumber();
+	}
+
+	private Player createPlayerFromString(String playerString) {
+		StringTokenizer tokenizer = new StringTokenizer(playerString);
 		String name = tokenizer.nextToken();
 		String colour = tokenizer.nextToken();
 		Player player = new Player(name, PlayerColour.valueOf(colour.toUpperCase()));
-		System.out.println("Controller: player Created");
-		game.getPlayers().add(player);
-		game.setGameState(game.getGameState().nextState());
-		System.out.println("Controller: state changed: new state");
-		System.out.println(game.getGameState());
-		System.out.println("Trying to create a new game");
-		
-		if (this.game.getGameState().equals(State.RUNNING)) {
-			Server.createANewGame();
-		}
-		
-		if (this.game.getGameState().equals(State.WAITINGFORPLAYERTHREE)) {
-			Server.tryToCreateANewGame();
-		}
-		return "Controller: Created player " + player.getMyName().toString() 
-				+ "\n --> CONNECTED TO GAME N" +game.getModelNumber()
-				+ "\n --> CONNECTED TO CONTROLLER N" +this.getControllerNumber();
+		return player;
+
+	}
+
+	private void sendModelToClients() {
+		System.out.println("Sono il controller numero" + this.getControllerNumber());
+		System.out.println("Sto inviando il model numero" + game.getModelNumber());
+		hashMap = new HashMap<>();
+		hashMap.put("model", game);
+		notifyMyObservers(hashMap);
 	}
 
 	public int getControllerNumber() {
@@ -190,7 +198,6 @@ public class Controller extends MyObservable implements MyObserver {
 				// che ti servono
 			}
 
-
 		} else {
 			// TODO: azione non valida
 
@@ -198,9 +205,11 @@ public class Controller extends MyObservable implements MyObserver {
 
 		return " sent";
 	}
-	
-	/**If the player wants to take a ventures card, this method let him choose which 
-	 * one of the double costs to take (if a double cost exists)*/
+
+	/**
+	 * If the player wants to take a ventures card, this method let him choose
+	 * which one of the double costs to take (if a double cost exists)
+	 */
 	private void handleVentures(MyObservable o, Map<String, Object> request, String tempZone, String tempFloor) {
 		TowerPlace placeRequested = (TowerPlace) this.game.getBoard().getZoneFromString(tempZone)
 				.getPlaceFromStringOrFirstIfZero(tempFloor);
