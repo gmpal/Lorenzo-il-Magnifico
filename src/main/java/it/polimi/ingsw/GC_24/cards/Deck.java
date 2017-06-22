afe6acca484c6693fd72060a86f18b78fb361bbf
giorgia.scappini@mail.polimi.it <giorgia.scappini@mail.polimi.it>
2017-06-23 01:00:50 +0200

    characters quasi fatti

--- src/main/java/it/polimi/ingsw/GC_24/cards/Deck.java
+++ src/main/java/it/polimi/ingsw/GC_24/cards/Deck.java
@@ -9,19 +9,25 @@ import java.util.Random;
 import com.google.gson.Gson;
 import it.polimi.ingsw.GC_24.board.Board;
 import it.polimi.ingsw.GC_24.devCardJsonFile.GsonBuilders;
+import it.polimi.ingsw.GC_24.effects.ChooseNewCard;
 import it.polimi.ingsw.GC_24.effects.CouncilPrivilege;
 import it.polimi.ingsw.GC_24.effects.Exchange;
 import it.polimi.ingsw.GC_24.effects.ExchangePackage;
+import it.polimi.ingsw.GC_24.effects.IncreaseDieValueCard;
 import it.polimi.ingsw.GC_24.effects.MoltiplicationCards;
+import it.polimi.ingsw.GC_24.effects.MoltiplicationPoints;
 import it.polimi.ingsw.GC_24.effects.PerformHarvest;
 import it.polimi.ingsw.GC_24.effects.PerformProduction;
 import it.polimi.ingsw.GC_24.effects.ValueEffect;
 import it.polimi.ingsw.GC_24.personalboard.PersonalBuildings;
+import it.polimi.ingsw.GC_24.personalboard.PersonalTerritories;
 import it.polimi.ingsw.GC_24.values.Coin;
 import it.polimi.ingsw.GC_24.values.MilitaryPoint;
 import it.polimi.ingsw.GC_24.values.SetOfValues;
+import it.polimi.ingsw.GC_24.values.Stone;
 import it.polimi.ingsw.GC_24.values.Value;
 import it.polimi.ingsw.GC_24.values.VictoryPoint;
+import it.polimi.ingsw.GC_24.values.Wood;
 
 public class Deck {
 	private List<Territories> deckTerritories = new ArrayList<>();
@@ -32,11 +38,11 @@ public class Deck {
 
 	// constructor
 	public Deck() throws IOException {
-		createDeck();
+		//createDeck();
 	}
 
 	// create 4 deck arrayList from 4 different file with Json
-	public void createDeck() throws IOException {
+/*	public void createDeck() throws IOException {
 		BufferedReader br;
 		Gson gson = GsonBuilders.getGsonWithTypeAdapters();
 		String line;
@@ -71,7 +77,7 @@ public class Deck {
 			this.deckLeaders.add(gson.fromJson(line, Leader.class));
 		}
 	}
-
+*/
 	// return a line of the file in buffer
 	public String getLine(BufferedReader br) throws IOException {
 		String line;
@@ -169,29 +175,35 @@ public class Deck {
 		BufferedReader br;
 		Gson gson = GsonBuilders.getGsonWithTypeAdapters();
 		String line;
-		Deck d=new Deck();
-		System.out.println(d);
-		/*SetOfValues set = new SetOfValues();
+		//Deck d=new Deck();
+		//System.out.println(d);
+		SetOfValues set = new SetOfValues();
 		SetOfValues set1 = new SetOfValues();
 		ValueEffect ve=new ValueEffect("value");
 		ve.setEffectValues(set);
-		set.setStones(new Stone(3));
-		set.setWoods(new Wood(1));
+		set.setCoins(new Coin(6));
 		ValueEffect ve1=new ValueEffect("value");
 		ve1.setEffectValues(set);
 		set1.setVictoryPoints(new VictoryPoint(5));
+		CouncilPrivilege privilege = new CouncilPrivilege("CouncilPrivilege", 3);
+		ChooseNewCard cnc = new ChooseNewCard("ChooseNewCard", "Venture", 6, null);
 		Buildings t=new Buildings("Mint", 5, "Building", set, ve1, null, new MoltiplicationCards("moltiplicationCard", new Coin(1), new PersonalBuildings()), null, 1);
-		System.out.println(gson.toJson(t));
-		
-		br = new BufferedReader(new
+		PerformHarvest ph = new PerformHarvest("Perform Harvest", 4);
+		PersonalTerritories pt = new PersonalTerritories();
+		MoltiplicationCards mc = new MoltiplicationCards("MoltiplicationCards", new VictoryPoint(2), pt);
+		MoltiplicationPoints mp = new MoltiplicationPoints("MoltiplicationPoints", new VictoryPoint(1), new MilitaryPoint(2));
+		IncreaseDieValueCard pe = new IncreaseDieValueCard("IncreaseDieValueCard", pt, 2, null, null);
+		Characters c = new Characters("General", "Character", set, mp, null, null, 3);
+		System.out.println(gson.toJson(c));
+	/*	br = new BufferedReader(new
 		FileReader("src/main/java/it/polimi/ingsw/GC_24/devCardJsonFile/provaC.json"));
 		String string;
 		string = br.readLine();
 		Buildings t1=gson.fromJson(string, Buildings.class);
 		ArrayList<Buildings> tx=new ArrayList<>();
 		tx.add(t1);
-		System.out.println(tx);
+		System.out.println(tx);*/
 
-		/*PermanentEffect pe = new IncreaseDieValueActivity("production", 3);
+		//PermanentEffect pe = new IncreaseDieValueActivity("production", 3);
 	}
 }
