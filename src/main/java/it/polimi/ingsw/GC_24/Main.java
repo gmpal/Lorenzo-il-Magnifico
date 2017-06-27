8270193b798282b6398c801e5fd3725c6bdac018
Gian Marco Paldino <g.m.pal95@gmail.com>
2017-06-27 18:36:34 +0200

    Revert "Controller UTF error"

--- src/main/java/it/polimi/ingsw/GC_24/Main.java
+++ src/main/java/it/polimi/ingsw/GC_24/Main.java
@@ -1,10 +1,15 @@
 package it.polimi.ingsw.GC_24;
 
-import it.polimi.ingsw.GC_24.values.SetOfValues;
+import it.polimi.ingsw.GC_24.frame.*;
 
 public class Main {
 	
 	public static void main(String[] args) {
-
+		
+		String s1 = "1";
+		String s2 = "0";
+		
+		
+		System.out.println(s2.compareTo(s1));
 	}
 }
